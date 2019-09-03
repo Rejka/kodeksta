56def0b61c0a8717d53fe914a02405e0c107d48f
jusondac <rejkamuhammad@gmail.com>
2019-09-03 20:23:25 +0700

    first commit

---
+++ rabit.py
@@ -0,0 +1,14 @@
+rabbit = 1
+on = rabbit
+die = 0
+
+# while rabbit has jumped 
+while(rabbit == 1):
+	print("rabbit its jumping")
+	jump = 10
+	for i in range(0,jump):
+		print("jumppp")
+		if i is 7:
+			print("i lose in seven")
+			break
+	rabbit = die
